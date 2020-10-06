4e118933e4f18be8a5cb6e51dba398d83ce8c7ab
Egor Nedelko <gojiramedia@gmail.com>
2020-10-06 16:00:56 +0300

    feat: split number (arg) into digits

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ module.exports = function toReadable (number) {
   let tens = ['ten ', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety ']
   let hundreds = ['one hundred ', 'two hundred ', 'three hundred ', 'four hundred ', 'five hundred ', 'six hundred ', 'seven hundred ', 'eight hundred ', 'nine hundred ']
   let thousands = ['one thousand ', 'two thousand ', 'three thousand ', 'four thousand ', 'five thousand ', 'six thousand ', 'seven thousand ', 'eight thousand ', 'nine thousand ']
+
+  let arr = []
+  let num = number
+  while (num > 0) {
+      arr.push(num % 10)
+      num = parseInt(num / 10) 
+  }
+  arr.reverse()
+  
   return result
 }
