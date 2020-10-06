bce699bd67c9c340ed9ca91e20e0d164e51097f3
Egor Nedelko <gojiramedia@gmail.com>
2020-10-06 16:13:40 +0300

    fix: change 'concat' method to interpolation for assembling result

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ module.exports = function toReadable (number) {
   let result = ''
   let zero = 'zero'
   let ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-  let tens = ['ten ', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety ']
-  let hundreds = ['one hundred ', 'two hundred ', 'three hundred ', 'four hundred ', 'five hundred ', 'six hundred ', 'seven hundred ', 'eight hundred ', 'nine hundred ']
-  let thousands = ['one thousand ', 'two thousand ', 'three thousand ', 'four thousand ', 'five thousand ', 'six thousand ', 'seven thousand ', 'eight thousand ', 'nine thousand ']
+  let tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let hundreds = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
+  let thousands = ['one thousand', 'two thousand', 'three thousand', 'four thousand', 'five thousand', 'six thousand', 'seven thousand', 'eight thousand', 'nine thousand']
 
   let arr = []
   let num = number
@@ -26,15 +26,20 @@ module.exports = function toReadable (number) {
   }
 
   if (number == 0) {
-    result.concat(zero) 
+    result = `${zero}`
+    //result.concat(zero) 
   } else if (ident === 'ones') {
-    result.concat(ones[number-1])
+    //result.concat(ones[number-1])
+    result = `${ones[number-1]}`
   } else if (ident === 'tens') {
-    result.concat(tens[arr[0]-1], ones[arr[1]-1])
+    //result.concat(tens[arr[0]-1], ones[arr[1]-1])
+    result = `${tens[arr[0]-1]} ${ones[arr[1]-1]}`
   } else if (ident === 'hundreds') {
-    result.concat(hundreds[arr[0]-1], tens[arr[1]-1], ones[arr[0]-1])
+    //result.concat(hundreds[arr[0]-1], tens[arr[1]-1], ones[arr[0]-1])
+    result = `${hundreds[arr[0]-1]} ${tens[arr[1]-1]} ${ones[arr[0]-1]}`
   } else if (ident === 'thousands') {
-    result.concat(thousands[arr[0]-1], hundreds[arr[1]-1], tens[arr[2]-1], ones[arr[3]-1])
+    //result.concat(thousands[arr[0]-1], hundreds[arr[1]-1], tens[arr[2]-1], ones[arr[3]-1])
+    result = `${thousands[arr[0]-1]} ${hundreds[arr[1]-1]} ${tens[arr[2]-1]} ${ones[arr[3]-1]}`
   }
 
   return result
