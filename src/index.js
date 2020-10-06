a6e2c41afae072adf711fca55676970a97a5ae26
Egor Nedelko <gojiramedia@gmail.com>
2020-10-06 15:59:42 +0300

    init: start rrs task 'human-readable-number'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+  let result = ''
+  let zero = 'zero'
+  let ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  let tens = ['ten ', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety ']
+  let hundreds = ['one hundred ', 'two hundred ', 'three hundred ', 'four hundred ', 'five hundred ', 'six hundred ', 'seven hundred ', 'eight hundred ', 'nine hundred ']
+  let thousands = ['one thousand ', 'two thousand ', 'three thousand ', 'four thousand ', 'five thousand ', 'six thousand ', 'seven thousand ', 'eight thousand ', 'nine thousand ']
+  return result
 }
