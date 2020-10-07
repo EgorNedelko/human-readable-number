9c8b96a71b133329757654b94af9d645b81e7f69
Egor Nedelko <gojiramedia@gmail.com>
2020-10-07 11:53:42 +0300

    feat: extend result assembly block to include 'tens part' of the number, exclude 'thousands' for redundancy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ module.exports = function toReadable (number) {
   let tens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteeb', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
   let dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   let hundreds = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
-  let thousands = ['one thousand', 'two thousand', 'three thousand', 'four thousand', 'five thousand', 'six thousand', 'seven thousand', 'eight thousand', 'nine thousand']
 
   let arr = []
   let num = number
@@ -27,9 +26,6 @@ module.exports = function toReadable (number) {
   } else if (number >= 100 && number <= 999) {
       ident = 'hundreds'
       tensPart = arr.slice(1)
-  } else if (number >= 1000 && number <= 9999) {
-      ident = 'thousands'
-      tensPart = arr.slice(2)
   }
 
   let isTens = false;
@@ -40,14 +36,19 @@ module.exports = function toReadable (number) {
   switch (ident) {
     case 'zero':
       result = `${zero}`
+      break
     case 'ones': 
       result = `${ones[number-1]}`
+      break
     case 'dozens':
-      result = `${tens[arr[0]-1]} ${ones[arr[1]-1]}`
+      if (isTens) {result = `${tens[tensPart[1]-1]}`}
+      else {result = `${dozens[arr[0]-1]} ${ones[arr[1]-1]}`}
+      break
     case 'hundreds': 
-      result = `${hundreds[arr[0]-1]} ${tens[arr[1]-1]} ${ones[arr[2]-1]}`
-    case 'thousands':
-      result = `${thousands[arr[0]-1]} ${hundreds[arr[1]-1]} ${tens[arr[2]-1]} ${ones[arr[3]-1]}`
+      if (isTens) {result = `${hundreds[arr[0]-1]} ${tens[tensPart[1]-1]}`}
+      else {result = `${hundreds[arr[0]-1]} ${dozens[arr[1]-1]} ${ones[arr[2]-1]}`}
+      break
   }
+
   return result
 }
