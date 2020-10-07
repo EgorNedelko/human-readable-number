bf06d2c14304ca57942b1ecd2a653a6433925d69
Egor Nedelko <gojiramedia@gmail.com>
2020-10-07 11:14:02 +0300

    feat: extend identification logic to check for 'tens part' of the number, add respective array

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ module.exports = function toReadable (number) {
   let result = ''
   let zero = 'zero'
   let ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-  let tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let tens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteeb', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   let hundreds = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
   let thousands = ['one thousand', 'two thousand', 'three thousand', 'four thousand', 'five thousand', 'six thousand', 'seven thousand', 'eight thousand', 'nine thousand']
 
@@ -15,31 +16,38 @@ module.exports = function toReadable (number) {
   arr.reverse()
   
   let ident = '';
-  if (number < 10) {
+  let tensPart = 0;
+  if (number == 0) {
+      ident = 'zero'
+  } else if (number < 10) {
       ident = 'ones'
   } else if (number >= 10 && number <= 99) {
-      ident = 'tens'
+      ident = 'dozens'
+      tensPart = arr
   } else if (number >= 100 && number <= 999) {
       ident = 'hundreds'
+      tensPart = arr.slice(1)
   } else if (number >= 1000 && number <= 9999) {
       ident = 'thousands'
+      tensPart = arr.slice(2)
   }
 
-  if (number == 0) {
+  let isTens = false;
+  if (tensPart[0] == 1 && tensPart[1] != 0) {
+    isTens = true;
+  }
+
+  if (ident == 'zero') {
     result = `${zero}`
-    //result.concat(zero) 
   } else if (ident === 'ones') {
-    //result.concat(ones[number-1])
     result = `${ones[number-1]}`
-  } else if (ident === 'tens') {
-    //result.concat(tens[arr[0]-1], ones[arr[1]-1])
+  } else if (ident === 'dozens') {
     result = `${tens[arr[0]-1]} ${ones[arr[1]-1]}`
   } else if (ident === 'hundreds') {
-    //result.concat(hundreds[arr[0]-1], tens[arr[1]-1], ones[arr[0]-1])
-    result = `${hundreds[arr[0]-1]} ${tens[arr[1]-1]} ${ones[arr[0]-1]}`
+    result = `${hundreds[arr[0]-1]} ${tens[arr[1]-1]} ${ones[arr[2]-1]}`
   } else if (ident === 'thousands') {
-    //result.concat(thousands[arr[0]-1], hundreds[arr[1]-1], tens[arr[2]-1], ones[arr[3]-1])
     result = `${thousands[arr[0]-1]} ${hundreds[arr[1]-1]} ${tens[arr[2]-1]} ${ones[arr[3]-1]}`
+
   }
 
   return result
